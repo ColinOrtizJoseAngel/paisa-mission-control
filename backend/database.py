3e37a5c68268634eea6c25e2393121d48a54ea64
ColinOrtizJoseAngel <aboouttwoo@gmail.com>
2025-08-30 13:12:57 -0600

    Configurar DATABASE_URL para despliegue en Render

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:admin123@localhost:5432/paisa_db"
+DATABASE_URL = "postgresql://paisa_db_user:sIcaNtX2J51h8poqqc5yRpbOnfq6ZcNx@dpg-d2pjv1f5r7bs739qaca0-a/paisa_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
